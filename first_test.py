ad7aa860ebddaa6c8f4620c7e2a9880ad917a95b
Hua-Yuan <e61983+git@gmail.com>
2015-08-21 02:08:51 +0800

    download csv file
    add other way to show content

--- first_test.py
+++ first_test.py
@@ -4,5 +4,14 @@
 import requests
 
 url = 'http://download.post.gov.tw/post/download/MailBox_All.csv'
+save_path = 'mailbox_location.csv'
 
-print requests.get(url).content
+with open(save_path, 'w') as f:
+    f.write(requests.get(url).content)
+
+with open(save_path, 'r')  as f:
+    print f.read()
+
+# with open(save_path,'w') as f:
+#   for line in f:
+#       print line   
