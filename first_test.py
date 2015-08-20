31a80614a9580535abe434b083a2731d1f57d69f
Hua-Yuan <e61983+git@gmail.com>
2015-08-21 02:20:00 +0800

    using csv to read file and using uniout to print  content

--- first_test.py
+++ first_test.py
@@ -2,16 +2,27 @@
 # encoding: utf-8
 
 import requests
+import csv
+import uniout
+from os.path import basename
+from os.path import exists
 
 url = 'http://download.post.gov.tw/post/download/MailBox_All.csv'
 save_path = 'mailbox_location.csv'
 
-with open(save_path, 'w') as f:
-    f.write(requests.get(url).content)
+def save(url, path = None):
+    if not path:
+        path = basename(url)
+    with open(path, 'w') as f:
+        f.write(requests.get(url).content)
 
-with open(save_path, 'r')  as f:
-    print f.read()
+if not exists(save_path):
+    save(url, save_path)
+
+with open(save_path)  as f:
+    for row in csv.reader(f):
+        print row
 
 # with open(save_path,'w') as f:
 #   for line in f:
-#       print line   
+#       print line
